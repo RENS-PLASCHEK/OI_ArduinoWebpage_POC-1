a9335058beeff4e975ca99c941406f7eedb9c74b
soundgarden 86 <311301@student.fontys.nl>
2021-04-07 10:20:41 +0200

    added serialport

--- app.js
+++ app.js
@@ -16,3 +16,55 @@ const parsers = SerialPort.parsers;
 const parser = new parsers.Readline({
     delimiter: '\r\n'
 });
+
+// opens up the port.
+// specify the location of the port.
+var port = new SerialPort('/dev/cu.usbmodem14401',{ 
+
+    // specify settings.
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+// Attach port to parser.
+port.pipe(parser);
+
+
+
+// Create server called 'app'.
+var app = http.createServer(function(req, res) {
+
+    // returns value and context index.html-file.
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+// opens up the port.
+// specify the location of the port.
+var port = new SerialPort('/dev/tty.wchusbserialfa1410',{ 
+
+    // specify settings.
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+// Attach port to parser.
+port.pipe(parser);
+
+
+
+// Create server called 'app'.
+var app = http.createServer(function(req, res) {
+
+    // returns value and context index.html-file.
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+
